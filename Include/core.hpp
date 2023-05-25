bf9c2917f6303e66fe64d4267382b13fbcd37ffe
Platykin <alves.igor@aluno.ufabc.edu.br>
2023-05-25 15:14:33 -0300

    Add some operator overloading

--- Include/core.hpp
+++ Include/core.hpp
@@ -3,19 +3,30 @@ using namespace InstantImpact;
 
 namespace InstantImpact {
     
-    class vector3{
+    class Vector3{
     public:
         real x, y, z;
-        vector3() 
+        Vector3() 
             : x(0), y(0), z(0){}
 
-        vector3(const real x_arg, const real y_arg, const real z_arg) 
+        Vector3(const real x_arg, const real y_arg, const real z_arg) 
             : x(x_arg), y(y_arg), z(z_arg){}
 
+        //Overloads
+        Vector3 operator*=(const real value){
+            x *= value;
+            y *= value;
+            z *= value;
+        }
+
+        void operator+=(const Vector3 &v3){
+            x += v3.x;
+            y += v3.y;
+            z += v3.z;
+        }
+
         void invert(){
-            x = -x;
-            y = -y;
-            z = -y;
+            x, y, z *= -1;
         }
 
         real magnitude() const{return real_sqrt(x*x+y*y+z*z);}
@@ -26,10 +37,7 @@ namespace InstantImpact {
             real l = magnitude();
             real arg_x, arg_y, arg_z;
             if (l > 0){
-                //I think of overloading the operator *= for this object intead of using vec3->arg in the future
-                this->x *= 1/l;
-                this->y *= 1/l;
-                this->z *= 1/l;
+                (*this) *= l;
             }
         }
     private:
