8366225ce80f96c8ab3ae390287c4da0b255ed8f
Platykin <igor.alves.pinto11@gmail.com>
2023-07-16 15:45:19 -0300

    Implement basic properties of particles and the beggining of it's implementation

--- Include/precision.hpp
+++ Include/precision.hpp
@@ -4,4 +4,6 @@ namespace InstantImpact {
     // If we need to switch type float to type double, we can change here (DONT'T FORGET TO CHANGE SQRTF TO SQRT)
    typedef float real;
    #define real_sqrt sqrtf
+   #define real_pow powf
+   
 }
